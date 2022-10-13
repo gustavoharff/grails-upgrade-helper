e50b3046d9d802dfbb7258c7385b13c8abb75c4f
Gustavo Harff <gustavo.harff@gmail.com>
2022-10-12 22:19:54 -0300

    Fetch diff

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import { Diff } from "./components/diff";
 
-const diff = `diff --git a/gradle.properties b/gradle.properties
-index f3eb227..2cc0d8a 100644
---- a/gradle.properties
-+++ b/gradle.properties
-@@ -1,4 +1,4 @@
--grailsVersion=5.2.3
-+grailsVersion=5.2.4
- grailsGradlePluginVersion=5.2.3
- groovyVersion=3.0.11
- gorm.version=7.3.2
-`;
-
 export function App() {
-  return <Diff diff={diff} />;
+  const [diff, setDiff] = useState<string | null>(null);
+
+  const [oldVersion, setOldVersion] = useState("5.2.2");
+  const [newVersion, setNewVersion] = useState("5.2.4");
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://raw.githubusercontent.com/gustavoharff/grails-upgrade-helper/diffs/diffs/${oldVersion}..${newVersion}.diff`
+      )
+      .then((response) => {
+        setDiff(response.data);
+      });
+  }, [oldVersion, newVersion]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={oldVersion}
+        onChange={(event) => setOldVersion(event.target.value)}
+      />
+      <input
+        type="text"
+        value={newVersion}
+        onChange={(event) => setNewVersion(event.target.value)}
+      />
+
+      {diff && <Diff diff={diff} />}
+    </div>
+  );
 }
